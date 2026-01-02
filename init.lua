aaf264ecb2ca1dd3039504b7a90908fa6920e037
Calvin <calvin71203@gmail.com>
2026-01-02 15:04:52 -0500

    daily

--- init.lua
+++ init.lua
@@ -792,6 +792,25 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    keys = {
+      { '<leader>DB', '<cmd>DBUIToggle<CR>', desc = 'Toggle DB UI' },
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 
   { -- Autocompletion
     'saghen/blink.cmp',
